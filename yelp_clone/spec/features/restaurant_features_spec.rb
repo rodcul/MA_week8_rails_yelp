2c33c037250b4400acfed04743ddb0dfa17f0ae6
smarbaf <smarbaf@hotmail.com>
2015-06-15 11:47:49 +0100

    Added a passing first test

--- yelp_clone/spec/features/restaurant_features_spec.rb
+++ yelp_clone/spec/features/restaurant_features_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 feature 'restaurants' do
   context 'no restaurants added yet' do
     scenario 'prompt to add a restaurant' do
-      visit('/restaurant')
-      expect(page).to have_content('No restaurants yet')
+      visit('/restaurants')
+      expect(page).to have_content('No restaurants yet!')
       expect(page).to have_link('Add a restaurant')
     end
   end
