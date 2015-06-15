ffd1b5b368bd58cf0e8d4bebd5f78bac9be714ae
smarbaf <smarbaf@hotmail.com>
2015-06-15 13:54:10 +0100

    Created form for adding a restaurant

--- yelp_clone/spec/features/restaurant_features_spec.rb
+++ yelp_clone/spec/features/restaurant_features_spec.rb
@@ -10,8 +10,18 @@ feature 'restaurants' do
     end
   end
 
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'MaccyDs'
+        click_button 'Create Restaurant'
+        expect(page).to have_content('MaccyDs')
+        expect(current_path).to eq '/restaurants'
+    end
+  end
   context 'restaurant added' do
-   before do
+    before do
       Restaurant.create(name: 'MaccyDs')
     end
     scenario 'display restaurants' do
@@ -19,5 +29,6 @@ feature 'restaurants' do
       expect(page).to have_content('MaccyDs')
       expect(page).not_to have_content('No restaurants yet!')
     end
+
   end
 end
