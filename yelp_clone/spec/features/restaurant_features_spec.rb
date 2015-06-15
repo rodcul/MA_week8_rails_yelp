538022eba66959ad1b768fe360a3fa31e6b54408
Alex Handy <handy437@gmail.com>
2015-06-15 11:00:58 +0100

    Failing first test

---
+++ yelp_clone/spec/features/restaurant_features_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'restaurants' do
+  context 'no restaurants added yet' do
+    scenario 'prompt to add a restaurant' do
+      visit('/restaurant')
+      expect(page).to have_content('No restaurants yet')
+      expect(page).to have_link('Add a restaurant')
+    end
+  end
+end
