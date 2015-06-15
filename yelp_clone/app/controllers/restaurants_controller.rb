9f45208f063d108d115833c670c0481d5b8aa6b3
Alex Handy <handy437@gmail.com>
2015-06-15 12:24:58 +0100

    Linked in Restaurant Model and passing tests

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -1,8 +1,5 @@
 class RestaurantsController < ApplicationController
   def index
-    raise 'Hello from the index action'
-  end
-  def show
-    render 'index'
+    @restaurants = Restaurant.all
   end
 end
--- yelp_clone/spec/features/restaurant_features_spec.rb
+++ yelp_clone/spec/features/restaurant_features_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
   context 'no restaurants added yet' do
     scenario 'prompt to add a restaurant' do
       visit('/restaurants')
       expect(page).to have_content('No restaurants yet!')
       expect(page).to have_link('Add a restaurant')
+    end
+  end
+
+  context 'restaurant added' do
+   before do
+      Restaurant.create(name: 'MaccyDs')
+    end
+    scenario 'display restaurants' do
+      visit('/restaurants')
+      expect(page).to have_content('MaccyDs')
+      expect(page).not_to have_content('No restaurants yet!')
     end
   end
 end
