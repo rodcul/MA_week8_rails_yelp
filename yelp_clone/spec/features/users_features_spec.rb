a4d7ec9ecb218dad065c6ec002c1875ac9434b29
Alex Handy <handy437@gmail.com>
2015-06-16 10:52:52 +0100

    Can sign in and out

--- yelp_clone/spec/features/users_features_spec.rb
+++ yelp_clone/spec/features/users_features_spec.rb
@@ -17,10 +17,11 @@ feature 'User sign in and out' do
   context "User is signed in and on the home-page" do
     before do
       visit ('/')
-      click_link('Sign in')
+      click_link('Sign up')
       fill_in("Email", with: 'test@example.com')
       fill_in("Password", with: 'testtest')
-      fill_in("Password Confirmation", with: 'testtest')
+      fill_in("Password confirmation", with: 'testtest')
+      click_button('Sign up')
     end
 
     it "should see a sign-out link" do
