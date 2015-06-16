f48f057c43dd2232eab98b794a1f8c5285696f4e
Alex Handy <handy437@gmail.com>
2015-06-16 10:43:35 +0100

    Added Devise and user tests

---
+++ yelp_clone/spec/features/users_features_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'User sign in and out' do
+  context 'User is not signed in and on the home-page' do
+    it "should see a sign in and sign up link" do
+      visit ('/')
+      expect(page).to have_link('Sign in')
+      expect(page).to have_link('Sign up')
+    end
+
+    it "should not be able to see sign-out link" do
+      visit('/')
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+
+  context "User is signed in and on the home-page" do
+    before do
+      visit ('/')
+      click_link('Sign in')
+      fill_in("Email", with: 'test@example.com')
+      fill_in("Password", with: 'testtest')
+      fill_in("Password Confirmation", with: 'testtest')
+    end
+
+    it "should see a sign-out link" do
+      visit('/')
+      expect(page).to have_link('Sign out')
+    end
+
+    it "should not see sign-in and sign-up" do
+      visit('/')
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
+end
