8d7be8d4da84a798cf5a6b4b0b4dfa80a4aa5c4f
Alex Handy <handy437@gmail.com>
2015-06-17 17:02:02 +0100

    Added image file upload with Paperclip

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -42,6 +42,6 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :description, :image)
   end
 end
